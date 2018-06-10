fe088d2ce0938868c037f6ec4ae46d543c31ec2c
Sébastien Fauvel <sebastien.fauvel@gmx.ch>
2018-06-10 10:30:49 +0200

    Website upload

--- website-hochladen.sh
+++ website-hochladen.sh
@@ -7,7 +7,9 @@ git pull
 cp -f ../* ./
 rm -f index.html
 mv Website.html index.html
-sed -e 's|</(HEAD|head)>|<link rel="stylesheet" type="text/css" href="style.css"></HEAD>|' -i '' index.html
+# Add stylesheet (case depending on the OpenOffice version)
+sed -e 's|</HEAD>|<link rel="stylesheet" type="text/css" href="style.css"></HEAD>|' -i '' index.html
+sed -e 's|</head>|<link rel="stylesheet" type="text/css" href="style.css"></head>|' -i '' index.html
 
 git add .
 git commit -m "Website upload"
