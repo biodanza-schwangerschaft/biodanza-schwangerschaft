0510a230193c971c5959407323e426f554e75375
Sébastien Fauvel <sebastien.fauvel@gmx.ch>
2018-06-10 10:29:51 +0200

    Website upload

--- website-hochladen.sh
+++ website-hochladen.sh
@@ -7,7 +7,7 @@ git pull
 cp -f ../* ./
 rm -f index.html
 mv Website.html index.html
-sed -e 's|</HEAD>|<link rel="stylesheet" type="text/css" href="style.css"></HEAD>|' -i '' index.html
+sed -e 's|</(HEAD|head)>|<link rel="stylesheet" type="text/css" href="style.css"></HEAD>|' -i '' index.html
 
 git add .
 git commit -m "Website upload"
