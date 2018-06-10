4e48cbdfe11996e01f865473b6f48c2b1e903864
Sébastien Fauvel <sebastien.fauvel@gmx.ch>
2018-06-10 16:05:11 +0200

    Website upload

--- website-hochladen.sh
+++ website-hochladen.sh
@@ -8,8 +8,9 @@ cp -f ../* ./
 rm -f index.html
 mv Website.html index.html
 # Add stylesheet (case depending on the OpenOffice version)
-sed -e 's|</HEAD>|<link rel="stylesheet" type="text/css" href="style.css"></HEAD>|' -i '' index.html
-sed -e 's|</head>|<link rel="stylesheet" type="text/css" href="style.css"></head>|' -i '' index.html
+tag='<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script",""//www.google-analytics.com/analytics.js","ga");ga("create","UA-120660038-1","biodanza-schwangerschaft.ch");ga("set","anonymizeIp",true);ga("require","displayfeatures");ga("send","pageview")</script><link rel="stylesheet" type="text/css" href="style.css">'
+sed -e "s~</HEAD>~$tag</HEAD>~" -i '' index.html
+sed -e "s~</head>~$tag</head>~" -i '' index.html
 
 git add .
 git commit -m "Website upload"
