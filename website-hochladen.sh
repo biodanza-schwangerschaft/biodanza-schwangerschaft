d8a9bdbec376d036bf20c4ddfada11a3dc39aa46
Sébastien Fauvel <sebastien.fauvel@gmx.ch>
2018-07-02 19:56:58 +0200

    Website upload

--- website-hochladen.sh
+++ website-hochladen.sh
@@ -9,7 +9,7 @@ rm -f index.html index.odt
 mv Website.html index.html
 mv Website.odt index.odt
 # Add stylesheet (case depending on the OpenOffice version)
-tag='<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","UA-120660038-1","biodanza-schwangerschaft.ch");ga("set","anonymizeIp",true);ga("require","displayfeatures");ga("send","pageview")</script><link rel="stylesheet" type="text/css" href="style.css">'
+tag='<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","UA-120660038-1","biodanza-schwangerschaft.ch");ga("set","anonymizeIp",true);ga("require","displayfeatures");ga("send","pageview")</script><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"><link rel="stylesheet" type="text/css" href="style.css">'
 sed -e "s~</HEAD>~$tag</HEAD>~" -i '' index.html
 sed -e "s~</head>~$tag</head>~" -i '' index.html
 
